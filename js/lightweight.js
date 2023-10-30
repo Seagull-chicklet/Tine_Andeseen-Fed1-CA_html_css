47652481d92c022cd79780f8ee0b124d0cfa0a69
Seagull-chicklet <126714065+Seagull-chicklet@users.noreply.github.com>
2023-10-30 10:44:32 +0100

    trying to get some js through

--- js/lightweight.js
+++ js/lightweight.js
@@ -1,15 +1,35 @@
 const url = "https://api.noroff.dev/api/v1/rainy-days/";
 
-const resultsContainer = document.querySelector(".results");
+const resultsLightweight = document;
 
 async function fetchJackets(){
     try{
         const response = await fetch(url);
         const results = await response.json();
-        console.log (results);
+
+        // console.log (results);
+
+        const jackets = results;
+        resultsLightweight.body.innerHTML = "";
+
+         const products =[];
+        for (let i = 0; i < jackets.length; i++){
+
+            if (jackets[i].tags.includes('womens')){
+                
+                products.push(jackets[i]);
+                
+            };
+        
+
+        //     resultsLightweight.innerHTML += `<div class="product-content">
+        //                                   <h2>${gender} + ${ jacketTitle}>/h2
+        //                                   </div>`;
+        }
+        console.log(products);
     }catch(error){
         console.log(error);
-        resultsContainer.innerHTML = error 
+        resultsLightweight.innerHTML = message("error",error  )  
     }
 }
 fetchJackets();
--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-const url = "https://api.noroff.dev/api/v1/rainy-days/";
+// const baseUrl = "https://api.noroff.dev/api/v1/rainy-days/";
 
-const resultsContainer = document.querySelector(".results");
+// const resultsContainer = document.querySelector(".results");
 
-async function getJackets(){
-    try{
-        const response = await fetch(url);
-        const results = await response.json();
-        console.log (results);
-    }catch(error){
-        console.log(error);
-        resultsContainer.innerHTML = error 
-    }
-}
-getJackets();
+// async function getJackets(){
+//     try{
+//         const response = await fetch(url);
+//         const results = await response.json();
+//         console.log (results);
+//     }catch(error){
+//         console.log(error);
+//         resultsContainer.innerHTML = error 
+//     }
+// }
+// getJackets();
