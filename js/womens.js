85087d2f6c022780739be0273dab0d072a136a68
Seagull-chicklet <126714065+Seagull-chicklet@users.noreply.github.com>
2023-11-07 11:04:42 +0100

    trying to figure it out js

--- js/womens.js
+++ js/womens.js
@@ -1,16 +1,16 @@
 const url = "https://api.noroff.dev/api/v1/rainy-days/";
 
-const resultsWomens = document;
+const productCard = document.querySelector(".product-card");
 
 async function fetchJackets() {
   try {
     const response = await fetch(url);
     const results = await response.json();
 
-    // console.log (results);
+    // console.log(results);
 
     const jackets = results;
-    resultsWomens.body.innerHTML = "";
+    productCard.innerHTML = "";
 
     const products = [];
     for (let i = 0; i < jackets.length; i++) {
@@ -18,31 +18,11 @@ async function fetchJackets() {
         products.push(jackets[i]);
       }
     }
-    function renderJackets(jacket) {
-      const section = document.querySelector(".lightweight-wrapping");
-
-      const jacketDiv = document.createElement("div");
-
-      const jacketTitle = document.createElement("h2");
-      jacketTitle.innerText = jackets.title;
-      const jacketContent = document.createElement("p");
-
-      jacketDiv.appendChild(jacketTitle);
-
-      section.appendChild(jacketDiv);
-    }
-
-    async function renderJackets() {
-      const jackets = await getJackets();
-
-      jackets.forEach((jacket) => renderJackets(jacket));
-    }
-    renderJackets();
-
     console.log(products);
   } catch (error) {
     console.log(error);
-    resultsWomens.innerHTML = message("error", error);
+
+    productCard.innerHTML = message("error", error);
   }
 }
-fetchJackets();
+fetchJackets(url);
